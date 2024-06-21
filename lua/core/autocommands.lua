d5d6acdc798901531386bde89d7b23a3d77f91af
Q <matthew@Qs-MacBook-Pro-Personal.local>
2024-06-21 10:34:43 +0900

    support run current file

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -16,35 +16,38 @@ local function show_in_floating_window(output)
 	local buf = vim.api.nvim_create_buf(false, true)
 	vim.api.nvim_buf_set_lines(buf, 0, -1, false, output)
 
-	local width = vim.o.columns - 10
-	local height = vim.o.lines - 10
+	-- local width = vim.o.columns - 10
+	-- local height = vim.o.lines - 10
 
-	local opts = {
-		style = "minimal",
-		relative = "editor",
-		width = width,
-		height = height,
+	local width = vim.api.nvim_win_get_width(0)
+	local height = vim.api.nvim_win_get_height(0)
+	vim.api.nvim_open_win(buf, true, {
+		relative = "win",
 		row = 5,
 		col = 5,
-	}
+		width = width - 10,
+		height = height - 10,
+	})
 
-	vim.api.nvim_open_win(buf, true, opts)
-
-	-- Close the window when any key is pressed
+	-- Close the window when <esc> or <ENTER> is pressed
 	vim.api.nvim_buf_set_keymap(buf, "n", "<esc>", ":q<CR>", { noremap = true, silent = true })
 	vim.api.nvim_buf_set_keymap(buf, "n", "<CR>", ":q<CR>", { noremap = true, silent = true })
 end
 
 -- Function to run the current Python file and display output
-function RunPythonFile()
+function RunCode()
 	if vim.bo.filetype == "python" then
 		vim.cmd("w")
 		local output = vim.fn.systemlist("python " .. vim.fn.shellescape(vim.fn.expand("%")))
 		show_in_floating_window(output)
+	elseif vim.bo.filetype == "go" then
+		vim.cmd("w")
+		local output = vim.fn.systemlist("go run " .. vim.fn.shellescape(vim.fn.expand("%")))
+		show_in_floating_window(output)
 	else
-		print("Not a Python file")
+		print("FileType Not Supported. Currently Support: Python, Golang")
 	end
 end
 
 -- Map <leader>r to the RunPythonFile function
-vim.api.nvim_set_keymap("n", "<leader>rc", ":lua RunPythonFile()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>rc", ":lua RunCode()<CR>", { noremap = true, silent = true })
