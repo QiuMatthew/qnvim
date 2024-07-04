8eb224349aa9fec76770f41b20bda8127146b968
Q <matthew@Qs-MacBook-Pro-Personal.local>
2024-07-05 05:57:50 +0900

    add leetcode plugin

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -315,4 +315,28 @@ require("lazy").setup({
 			require("dap").set_log_level("DEBUG")
 		end,
 	},
+
+	-- Leetcode
+	{
+		"kawre/leetcode.nvim",
+		build = ":TSUpdate html",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim", -- required by telescope
+			"MunifTanjim/nui.nvim",
+
+			-- optional
+			"nvim-treesitter/nvim-treesitter",
+			"rcarriga/nvim-notify",
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			-- configuration goes here
+			cn = { -- leetcode.cn
+				enabled = false, ---@type boolean
+				translator = true, ---@type boolean
+				translate_problems = true, ---@type boolean
+			},
+		},
+	},
 })
