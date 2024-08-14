ce9b3abfa56634311b062c85371feffa846047e1
QiuMatthew <uzenkyu@gmail.com>
2024-08-15 07:03:10 +0900

    support template file

--- lua/plugins/competitest.lua
+++ lua/plugins/competitest.lua
@@ -3,7 +3,12 @@ return {
 		"xeluxee/competitest.nvim",
 		dependencies = "MunifTanjim/nui.nvim",
 		config = function()
-			require("competitest").setup()
+			require("competitest").setup({
+				template_file = {
+					cpp = "~/Projects/CompetitiveProgramming/Templates/template.cpp",
+					py = "~/Projects/CompetitiveProgramming/Templates/template.py",
+				},
+			})
 		end,
 	},
 }
