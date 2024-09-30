c40b4900b82cf654ba70088f2a8a18c83ec98014
QiuMatthew <uzenkyu@gmail.com>
2024-09-30 15:39:27 +0800

    put git blame info into status line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,12 +3,23 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup {
-                options = {
-                    theme = "auto",
-                },
-            }
-        end
+		config = function()
+			vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+			local git_blame = require("gitblame")
+
+			require("lualine").setup({
+				sections = {
+					lualine_c = {
+						{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+					},
+				},
+			})
+
+			require("lualine").setup({
+				options = {
+					theme = "auto",
+				},
+			})
+		end,
 	},
 }
